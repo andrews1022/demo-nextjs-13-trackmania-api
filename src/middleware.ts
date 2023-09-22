949f08e5926c1523e47235deb0183afb059990f3
Andrew Shearer <andrewshearerdev@gmail.com>
2023-09-21 18:40:19 -0700

    updated prisma client to use prisma client data proxy to run on the edge
    - updated project name in package.json
    - updated title in rootlayout metadata

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,18 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import prisma from "./lib/prisma";
 
 // This function can be marked `async` if using `await` inside
-const middleware = (request: NextRequest) => {
-  console.log("middleware running!!!");
+const middleware = async (request: NextRequest) => {
+  // console.log("middleware running!!!");
+
   // return NextResponse.json(request)
+  const apiTokenTest = await prisma.apiToken.findUnique({
+    where: {
+      tokenId: "c1Z1oSjpTT3REWxoeuK0xcWQBj"
+    }
+  });
+  console.log("apiTokenTest: ", apiTokenTest);
 };
 
 export { middleware };
